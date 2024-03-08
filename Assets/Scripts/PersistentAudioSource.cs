c5a621a891605682d5ce4f981c5bd9e424e03916
Benjamin Farrington <benjaminfarrington36@gmail.com>
2024-03-08 02:31:29 -0500

    Added bgm and lowered volume of game

---
+++ Assets/Scripts/PersistentAudioSource.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PersistentAudioSource : MonoBehaviour
+{
+    private static PersistentAudioSource instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject); // Destroy this GameObject if an instance already exists.
+        }
+    }
+}
